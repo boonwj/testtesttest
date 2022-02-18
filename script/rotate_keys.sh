ce3d74ce0dc703050c40176ed6faaa5ff317d1b8
Boon Wenjie <boonwenjie@gmail.com>
2022-02-18 14:37:54 +0800

    test: make script break

--- script/rotate_keys.sh
+++ script/rotate_keys.sh
@@ -2,25 +2,21 @@
 
 function rotate_keys {
     kv_name=$1
-    kv_resource_group=$2
-    kv_secret_name=$3
-    sst_name=$4
-    sst_resource_group=$5
+    kv_secret_name=$2
+    sst_name=$3
+    sst_resource_group=$4
 
-    echo "Key vault: ${kv_name}"
-    echo "Key vault RG: ${kv_resource_group}"
-    echo "Key vault secret name: ${kv_secret_name}"
-    echo "Storage account: ${sst_name}"
-    echo "Storage account RG: ${sst_resource_group}"
+    echo "Key vault: ${kv_name}, Secret Name: ${kv_secret_name}"
+    echo "Storage account: ${sst_name}, Resource Group: ${sst_resource_group}"
 
     echo "- Refreshing and updating secret with secondary key"
     key2=$(az storage account keys renew --account-name "${sst_name}" --resource-group "${sst_resource_group}" --key secondary --query "[?keyName=='key2'].value" -o tsv)
     az keyvault secret set --name "${kv_secret_name}" --vault-name "${kv_name}" --description "Access key for ${sst_name}" --value "${key2}" -o none
 
-    echo "- Refreshing and updatign secret with primary key"
+    echo "- Refreshing and updating secret with primary key"
     key1=$(az storage account keys renew --account-name "${sst_name}" --resource-group "${sst_resource_group}" --key primary --query "[?keyName=='key1'].value" -o tsv)
-    az keyvault secret set --name "${kv_secret_name}" --vault-name "${kv_name}" --description "Access key for ${sst_name}" --value "${key1}" -o none
+    az keyvault secret set --name "${kv_secret_name}" --vault-name "${kv_name}" --description "Access key for ${sst_name}" --value "${key1}"
 }
 
-rotate_keys "kv-sst-rotate" "rg-kv-sst-rotate-test" "sst-account001-key" "sstaccount001" "rg-kv-sst-rotate-test"
-rotate_keys "kv-sst-rotate" "rg-kv-sst-rotate-test" "sst-account002-key" "sstaccount002" "rg-kv-sst-rotate-test"
+rotate_keys "kv-sst-rotate" "sst-account001-key" "sstaccount001" "rg-kv-sst-rotate-test"
+rotate_keys "kv-sst-rotate-fail" "sst-account002-key" "sstaccount002" "rg-kv-sst-rotate-test"
